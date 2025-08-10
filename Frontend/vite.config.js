6fb5a9e2b78d7abcea63e9ae995f8acc1cf8b5e7
Md Mahedi Hassan <hmahedi906@gmail.com>
2025-08-10 15:22:27 +0600

    Version 3: Deployment

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // or use '0.0.0.0'
+    host: true,
+    allowedHosts: ['mygpt-frontend-sfsd.onrender.com'], // or use '0.0.0.0'
     port: 5173, // optional
   },
 });
